2f4066b8f2b3dd7042a6d9d7891fe1e2cd911df6
bingoori <woosungdoo@gmail.com>
2016-03-10 15:28:29 +0900

    NumGolf

--- javaStmt/src/whileloop/NumGolfMain.java
+++ javaStmt/src/whileloop/NumGolfMain.java
@@ -15,15 +15,14 @@ public class NumGolfMain {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("컴퓨터 랜덤 수 발생");
 		bean.setCom();
-		
-		
 		while(bean.isFlag() == false)
 		{
-			
-			if(bean.getCom() == scanner.nextInt())
+			int input = scanner.nextInt();
+			if(bean.getCom() == input)
 			{
 				System.out.println("정답");
 				bean.setFlag(true);
+				bean.setPlayer(input);
 			}else
 			{
 				bean.setCount((bean.getCount()+1));
